b22fb02a2bc7a898c4ab0c99472383a7a27fae3c
Alex Yu <j620656786206@gmail.com>
2017-12-03 14:40:28 -0600

    The_Complete_Android_N_Developer_Course - 114. App: Notes - first commit

---
+++ app/src/main/java/com/alexyu/notesapp/NoteEditorActivity.java
@@ -0,0 +1,43 @@
+package com.alexyu.notesapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+        int noteId = intent.getIntExtra("noteId", -1);
+
+        if(noteId != -1) {
+            editText.setText(MainActivity.notes.get(noteId));
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
