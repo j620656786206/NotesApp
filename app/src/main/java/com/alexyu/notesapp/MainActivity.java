4eaca3dc082086fd85f0cf14528db2b703443468
Alex Yu <j620656786206@gmail.com>
2017-12-03 15:16:34 -0600

    The_Complete_Android_N_Developer_Course - 114. App: Notes - feature "add note" added

--- app/src/main/java/com/alexyu/notesapp/MainActivity.java
+++ app/src/main/java/com/alexyu/notesapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.alexyu.notesapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,6 +16,29 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.add_note) {
+            Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+
+            startActivity(intent);
+            return true;
+        }
+
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         notes.add("Example note");
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
 
         listView.setAdapter(arrayAdapter);
 
